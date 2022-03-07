f2082fdbde6b44e4b6e94b048534a09f672010cb
geonye <rjsdp@naver.com>
2022-03-07 22:31:19 +0900

    - lombok라이브러리 사용 - board-Mapper.xml 쿼리수

--- test1/src/main/java/com/crud/domain/BoardVO.java
+++ test1/src/main/java/com/crud/domain/BoardVO.java
@@ -1,7 +1,13 @@
 package com.crud.domain;
 
+import lombok.Data;
+import lombok.ToString;
+
 import java.util.Date;
 
+
+@ToString
+@Data
 public class BoardVO {
 	
 	private Integer bno;
@@ -10,46 +16,4 @@ public class BoardVO {
 	private String writer;
 	private Date regdate;
 	private int viewcnt;
-	public Integer getBno() {
-		return bno;
-	}
-	public void setBno(Integer bno) {
-		this.bno = bno;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public Date getRegdate() {
-		return regdate;
-	}
-	public void setRegdate(Date regdate) {
-		this.regdate = regdate;
-	}
-	public int getViewcnt() {
-		return viewcnt;
-	}
-	public void setViewcnt(int viewcnt) {
-		this.viewcnt = viewcnt;
-	}
-	@Override
-	public String toString() {
-		return "BoardVO [bno=" + bno + ", title=" + title + ", content=" + content + ", writer=" + writer + ", regdate="
-				+ regdate + ", viewcnt=" + viewcnt + "]";
-	}
-	
 }
